177f5345a3e48ba2178429db311771eaacae0be2
iamnotfelix <andrei.iliescu03@gmail.com>
2023-04-26 14:17:44 +0300

    Created seeding script.

---
+++ sql/sample.sql
@@ -0,0 +1,14 @@
+use urlcollection;
+
+insert into categories (name) values 
+    ("Entertainment"), 
+    ("Work"), 
+    ("Study"); 
+
+insert into urls (url, description, category) values 
+    ("https://www.youtube.com", "Desctiption1", 1),
+    ("https://www.instagram.com", "Desctiption2", 1),
+    ("https://www.cs.ubbcluj.ro", "Desctiption3", 2),
+    ("https://www.cs.ubbcluj.ro/~forest/impulse", "Desctiption4", 2),
+    ("https://getbootstrap.com", "Desctiption5", 3),
+    ("https://code.visualstudio.com/docs", "Desctiption6", 3);
